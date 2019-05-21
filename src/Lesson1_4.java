038e5c83e56a09b4fb134d14c3d7a257a9c8e42e
Ki4elutevs <kovalonoka.svetlana@gmail.com>
2019-05-21 19:23:15 +0300

    added file

---
+++ src/Lesson1_4.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lesson1_4 {
+    public static void main(String[]args){
+        Random random = new Random();
+        int guess = random.nextInt(100)+1;
+        int enter;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(guess);
+        System.out.print("Please enter number: ");
+        enter = scanner.nextInt();
+
+        do {
+            if (enter>guess){
+                System.out.print("Smaller. Please enter number: ");
+                enter = scanner.nextInt();
+            } else if (enter<guess){
+                System.out.print("Bigger. Please enter number: ");
+                enter = scanner.nextInt();
+            }
+
+        } while (enter!=guess);
+        System.out.println("Congratulations!");
+
+
+    }
+
+}
